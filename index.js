a91b9b8a689004576c2286abde94b10fae9e93ed
Isaac Fdez <sakro.developer@gmail.com>
2018-08-18 15:44:56 +0100

    added script to create a Dockerfile customized to client project

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const { configureAWS } = require('./src/aws/configureAWS');
 const { createInstance } = require('./src/aws/createInstance');
 const { executeScript } = require('./src/tools/scriptsExecuter');
 const { getProjectName } = require('./src/tools/getProjectName');
+// Project to test 
+const gitUrl = 'https://github.com/sakdev/TheFox.git';
 
 configureAWS();
 createInstance().then( data => {
   console.log(`Ec2 instance (${ data.instanceId }) created and asocciated to this public ip: ${ data.publicIp }`);
-  //executeScript('scripts/hi.sh');
+  executeScript(`/home/deployme/scripts/createDockerFile.sh ${gitUrl} ${ getProjectName(gitUrl) }`);
+  
 })
