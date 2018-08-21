a755768d6d16a3b989c72292345e0033453cf27a
Samuel <worldf1gc@gmail.com>
2018-08-21 18:53:26 +0100

    Revert "Automatic ssh"
    This reverts commit 83fadb7f29b5513810dec87629fb752af326ed9e.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,4 +11,4 @@ tar -czf setup-files.tar.gz client-setup-files
 echo "scp -i /home/private/deploymepairkeys.pem setup-files.tar.gz ubuntu@$1:/tmp"
 scp -i /home/private/deploymepairkeys.pem setup-files.tar.gz ubuntu@$1:/tmp
 rm setup-files.tar.gz
-ssh -i /home/private/deploymepairkeys.pem -t -o "StrictHostKeyChecking no" ubuntu@$1 $uncompress$deleteCompressed$updateApt$installDocker$buildDocker$runDocker'; bash -l' 
+ssh -i /home/private/deploymepairkeys.pem -t ubuntu@$1 $uncompress$deleteCompressed$updateApt$installDocker$buildDocker$runDocker'; bash -l' 
