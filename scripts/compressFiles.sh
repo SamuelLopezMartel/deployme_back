c5a332903bbd9df8359df36af391a5b68c00a0e9
Isaac Fdez <sakro.developer@gmail.com>
2018-08-23 14:54:14 +0100

    +deploy client front project automatized

---
+++ scripts/compressFiles.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+tar -czf setup-files.tar.gz client-setup-files
--- src/tools/deployClientProject.js
+++ src/tools/deployClientProject.js
@@ -1,6 +1,8 @@
 const { createInstance } = require('../aws/createInstance');
-const { executeScript } = require('./scriptsExecuter');
 const { getProjectName } = require('./getProjectName');
+const { executeScript } = require('./scriptsExecuter');
+const shell = require('shelljs');
+var rexec = require('remote-exec');
 
 // Project to test 
 const gitUrl = 'https://github.com/sakdev/TheFox.git';
@@ -9,6 +11,52 @@ module.exports.deployClientProject = (gitUrl) => {
   createInstance().then( data => {
     console.log(`Ec2 instance (${ data.instanceId }) created and asocciated to this public ip: ${ data.publicIp }`);
     executeScript(`/home/deployme/scripts/createDockerFile.sh ${gitUrl} ${ getProjectName(gitUrl) }`);
-    executeScript(`/home/deployme/scripts/deploy.sh ${data.publicIp}`);
+    executeScript(`/home/deployme/scripts/compressFiles.sh`);
+    deploy(data.publicIp);
   })
+}
+
+function deploy(publicIp){
+  if( shell.exec(`ssh -tt -i /home/deployme/private/deploymepairkeys.pem -t -o "StrictHostKeyChecking no" ubuntu@${publicIp} "exit; bash -l"`).code !== 0){
+    console.log("Problems with ssh1");
+    shell.exit(1);
+  }else{
+    console.log("hice el primer ssh")
+    if( shell.exec(`scp -i /home/deployme/private/deploymepairkeys.pem setup-files.tar.gz ubuntu@${publicIp}:/tmp`).code !== 0){
+      console.log("Problems with scp");
+      shell.exit(1);
+    }else{
+      console.log("pasé el fichero")
+      shell.exec("rm setup-files.tar.gz");
+      remoteExecuter(publicIp);
+    }
+  }
+}
+
+function remoteExecuter(publicIp){
+  const connection_options = {
+    port: 22,
+    username: 'ubuntu',
+    privateKey: require('fs').readFileSync('./private/deploymepairkeys.pem'),
+    passphrase: ''
+  };
+  
+  const hosts = [ publicIp ];
+
+  const cmds = [ 
+    'tar -xzf /tmp/setup-files.tar.gz',
+    'sudo apt update',
+    'rm /tmp/setup-files.tar.gz',
+    'sudo apt install docker.io -y',
+    'sudo docker build -t image-front ./client-setup-files/',
+    'sudo docker run -p 80:80 image-front'
+  ];
+
+  rexec(hosts, cmds, connection_options, function(err){
+      if (err) {
+          console.log(err);
+      } else {
+          console.log('Great Success!!');
+      }
+  });
 }
